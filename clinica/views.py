ede59fe31554e966cd8e465163e170aa5839e233
BrandonSanchez2005 <bran19829@gmail.com>
2026-09-21 04:25:31 -0600

    Arreglo de error en función POST de api_mascotas

--- clinica/views.py
+++ clinica/views.py
@@ -59,6 +59,20 @@ def api_mascotas(request):
             "resultados": serializer.data
         })
 
+    serializer = MascotaSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED
+        )
+
+    return Response(
+        serializer.errors,
+        status=status.HTTP_400_BAD_REQUEST
+    )
+
 
 @api_view(["GET", "PUT", "PATCH", "DELETE"])
 def detalle_mascota(request, pk):
